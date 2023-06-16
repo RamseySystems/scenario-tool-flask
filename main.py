a81a6aabeb84e945a274f366c1bd5284a7112e62
charliemccay <charlie@ramseysystems.co.uk>
2023-06-16 17:31:40 +0100

    added top level error handler and a template for errors to be displayed

--- main.py
+++ main.py
@@ -27,73 +27,80 @@ def main():
 
 @app.route('/upload', methods=['POST'])
 def upload_file():
-    fn.clear_dir(UPLOAD_FOLDER)
-    fn.clear_dir(OUTPUT_FOLDER)
-    fn.clear_dir(ZIP_FOLDER)
-    shutil.copytree(f'{CURRENT_DIR}/data view website contents', f'{OUTPUT_FOLDER}/website', dirs_exist_ok=True)
+    try:
+        fn.clear_dir(UPLOAD_FOLDER)
+        fn.clear_dir(OUTPUT_FOLDER)
+        fn.clear_dir(ZIP_FOLDER)
+        shutil.copytree(f'{CURRENT_DIR}/data view website contents', f'{OUTPUT_FOLDER}/website', dirs_exist_ok=True)
 
-    if request.method == 'POST':
-        if 'file' not in request.files:
-            print('No file part')
-            return redirect(request.url)
-        
-
-
-        # get files from request 
-        files = request.files.getlist('file')
-
-        # check file types
-
-        # save files
-        for file in files:
-            if file.filename == '':
-                print('No selected file')
-                return redirect(url_for('main'))
-            file.save(os.path.join(UPLOAD_FOLDER, file.filename))
-
-        # personae
-        personae = []
-        for name in os.listdir(UPLOAD_FOLDER):
-            personae.append(name[:-5])
-
-        # process files
-        summaries = []
-        for file in os.listdir(UPLOAD_FOLDER):
-            summary = fn.process_file(file, OUTPUT_FOLDER, UPLOAD_FOLDER, f'{CURRENT_DIR}/standards')
-            with open(f'{OUTPUT_FOLDER}/{file[:-5]}/summary.json', 'w') as f:
-                json.dump(summary, f, indent=4)
-
-            summaries.append(summary)
-
-        # create dropdown gen
-        # render log 
-        env = Environment(loader=FileSystemLoader('templates'))
-        template = env.get_template('dropdown-gen-template.jinja')
-        output = template.render(array=personae)
-
-        with open(f'{OUTPUT_FOLDER}/website/JS/dropdown-gen.js', 'w') as f:
-            f.write(output)
-
-        template = env.get_template('index_template.jinja')
-        output = template.render(personae=personae)
-
-        with open(f'{OUTPUT_FOLDER}/website/index.html', 'w') as f:
-            f.write(output)
+        if request.method == 'POST':
+            if 'file' not in request.files:
+                print('No file part')
+                return redirect(request.url)
+            
 
 
-        # zip archive output folder
-        shutil.make_archive(f'{ZIP_FOLDER}/output', 'zip', OUTPUT_FOLDER)
+            # get files from request 
+            files = request.files.getlist('file')
 
-        fn.create_false_path_excel(OUTPUT_FOLDER, summaries)
+            # check file types
 
-    return render_template('file_download.html', summaries = summaries)
+            # save files
+            for file in files:
+                if file.filename == '':
+                    print('No selected file')
+                    return redirect(url_for('main'))
+                file.save(os.path.join(UPLOAD_FOLDER, file.filename))
+
+            # personae
+            personae = []
+            for name in os.listdir(UPLOAD_FOLDER):
+                personae.append(name[:-5])
+
+            # process files
+            summaries = []
+            for file in os.listdir(UPLOAD_FOLDER):
+                summary = fn.process_file(file, OUTPUT_FOLDER, UPLOAD_FOLDER, f'{CURRENT_DIR}/standards')
+                with open(f'{OUTPUT_FOLDER}/{file[:-5]}/summary.json', 'w') as f:
+                    json.dump(summary, f, indent=4)
+
+                summaries.append(summary)
+
+            # create dropdown gen
+            # render log 
+            env = Environment(loader=FileSystemLoader('templates'))
+            template = env.get_template('dropdown-gen-template.jinja')
+            output = template.render(array=personae)
+
+            with open(f'{OUTPUT_FOLDER}/website/JS/dropdown-gen.js', 'w') as f:
+                f.write(output)
+
+            template = env.get_template('index_template.jinja')
+            output = template.render(personae=personae)
+
+            with open(f'{OUTPUT_FOLDER}/website/index.html', 'w') as f:
+                f.write(output)
+
+
+            # zip archive output folder
+            shutil.make_archive(f'{ZIP_FOLDER}/output', 'zip', OUTPUT_FOLDER)
+
+            fn.create_false_path_excel(OUTPUT_FOLDER, summaries)
+
+        return render_template('file_download.html', summaries = summaries)
+    except Exception as e:
+        print(e)
+        return render_template('error.html', error=e)
 
 
 @app.route('/download', methods=['GET', 'POST'])
 def download():
  #   result = send_from_directory(directory=ZIP_FOLDER, path=f'{ZIP_FOLDER}/output.zip', filename='output.zip')
-    return  send_file(f'{ZIP_FOLDER}/output.zip', as_attachment=True)
-
+    try:
+        return  send_file(f'{ZIP_FOLDER}/output.zip', as_attachment=True)
+    except Exception as e:
+        print(e)
+        return render_template('error.html', error=e)   
 
 
 if __name__ == '__main__':
