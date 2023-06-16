cc13a510d8bb7ce7846e8b6a5fdbf6cfe79d2f5b
charliemccay <charlie@ramseysystems.co.uk>
2023-06-16 09:34:03 +0100

    fixed requirements moved file clearing from main to upload to avoid need for refresh

--- main.py
+++ main.py
@@ -20,16 +20,18 @@ CORS(app)
 
 @app.route('/')
 def main():
-    fn.clear_dir(UPLOAD_FOLDER)
-    fn.clear_dir(OUTPUT_FOLDER)
-    fn.clear_dir(ZIP_FOLDER)
-    shutil.copytree('data view website contents', f'{OUTPUT_FOLDER}/website', dirs_exist_ok=True)
+
     return render_template('index.html')
 
 
 
 @app.route('/upload', methods=['POST'])
 def upload_file():
+    fn.clear_dir(UPLOAD_FOLDER)
+    fn.clear_dir(OUTPUT_FOLDER)
+    fn.clear_dir(ZIP_FOLDER)
+    shutil.copytree('data view website contents', f'{OUTPUT_FOLDER}/website', dirs_exist_ok=True)
+    
     if request.method == 'POST':
         if 'file' not in request.files:
             flash('No file part')
